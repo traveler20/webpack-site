5fafea901e8e94af2125796f728e3ce06217c099
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2021-08-05 10:51:49 +0900

    🎉Initial Commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// import 文を使って sub.js ファイルを読み込む。
+import { hello } from "./js/sub";
+// import文を使ってSassファイルを読み込む。
+import "./scss/style.scss";
+
+// sub.jsに定義されたJavaScriptを実行する。
+hello();
---
+++ src/js/sub.js
@@ -0,0 +1,9 @@
+// export文を使ってhello関数を定義する。
+export function hello() {
+	console.log("helloメソッドが実行された。webpack-dev-serverを使っています。");
+}
+console.log("aaa");
+
+// "scripts": {
+//   "test": "echo \"Error: no test specified\" && exit 1"
+// },
