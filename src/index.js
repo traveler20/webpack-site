f3f8d83b120e2e876a7a820a38ad38e09bdaba9d
traveler20 <tabibito.traveler.sea.1039@gmail.com>
2022-05-07 07:15:50 +0900

    📝Modify

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import { Hello } from "./hello";
-
-const jiro = new Hello("Jiro");
-jiro.greet();
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
 /** ↓ エディタで補完を効かせるための JSDoc */
 /** @type {import('webpack').Configuration} */
+
+// Node.js の path モジュールをインポート
+const path = require("node:path");
+
 module.exports = {
 	module: {
 		rules: [
 			{
-				// 拡張子 js のファイル（正規表現）
-				test: /\.js$/,
-				// ローダーの指定
+				test: /\.jsx?$/,
 				loader: "babel-loader",
 			},
+			{
+				// 拡張子 css のファイル（正規表現）
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
 		],
+	},
+	// 依存関係解決に参照するファイルの拡張子を指定
+	resolve: {
+		extensions: [".js", ".json", ".jsx"],
+	},
+	entry: "./src/main.jsx",
+	output: {
+		// ファイル名
+		filename: "bundle.js",
+		// 出力するフォルダ
+		path: path.resolve(__dirname, "dist"),
 	},
 	devtool: "source-map",
 	devServer: {
